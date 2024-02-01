e8309de967787286e760b4d9059a0f409f7155c7
Raven Barrogo <barrogoraven@gmail.com>
2024-02-01 12:25:48 +0800

    CHANGE: pinia option stores to setup stores

--- src/stores/movies/index.ts
+++ src/stores/movies/index.ts
@@ -1,44 +1,38 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 import { API } from '@/services'
 import type { Movie } from '@/services/movies/types'
 import type { AxiosError } from 'axios'
 import { handleApiError } from '@/composables/handleApiError'
 
-interface State {
-  movies: Movie[]
-}
+export const useMovieStore = defineStore('movie', () => {
+  const movies = ref<Movie[]>([])
 
-export const useMovieStore = defineStore('movie', {
-  state: (): State => {
-    return {
-      movies: []
-    }
-  },
-  actions: {
-    async getMovies(): Promise<void> {
-      try {
-        const { data } = await API.movies.getMovies()
-        this.initMovies(data.results)
-      } catch (error) {
-        const _error = error as AxiosError<string>
-        handleApiError(_error.response?.status)
-      }
-    },
-    initMovies(data: Movie[]) {
-      const movies = data.map((item: Movie) => {
-        return {
-          id: item.id,
-          title: item.title,
-          overview: item.overview,
-          release_date: item.release_date,
-          vote_average: item.vote_average,
-          poster_path: item.poster_path,
-          backdrop_path: item.backdrop_path,
-          genre_ids: item.genre_ids
-        }
-      })
-
-      this.movies = movies
+  async function getMovies(): Promise<void> {
+    try {
+      const { data } = await API.movies.getMovies()
+      initMovies(data.results)
+    } catch (error) {
+      const _error = error as AxiosError<string>
+      handleApiError(_error.response?.status)
     }
   }
+
+  function initMovies(data: Movie[]) {
+    const results = data.map((item: Movie) => {
+      return {
+        id: item.id,
+        title: item.title,
+        overview: item.overview,
+        release_date: item.release_date,
+        vote_average: item.vote_average,
+        poster_path: item.poster_path,
+        backdrop_path: item.backdrop_path,
+        genre_ids: item.genre_ids
+      }
+    })
+
+    movies.value = results
+  }
+  return { movies, getMovies }
 })
