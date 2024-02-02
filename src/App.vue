406db13c0bdf2099f662f4afa705907bcec6f282
Raven Barrogo <barrogoraven@gmail.com>
2024-02-02 11:34:00 +0800

    ADD: footer :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
   <main>
     <RouterView />
   </main>
+  <TheFooter />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
 import TheHeader from '@/layouts/TheHeader.vue'
+import TheFooter from '@/layouts/TheFooter.vue'
 </script>
---
+++ src/layouts/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="self-end py-8">
+    <BaseContainer>
+      <div class="flex items-center justify-center gap-x-1 text-custom-foreground">
+        <Copyright :size="18" />
+        <p class="text-lg">
+          2024&nbsp;<span class="text-xl font-medium tracking-tighter text-custom-primary"
+            >sinehan</span
+          >. All rights reserved.
+        </p>
+      </div>
+    </BaseContainer>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import BaseContainer from '@/components/ui/container/BaseContainer.vue'
+import { Copyright } from 'lucide-vue-next'
+</script>
