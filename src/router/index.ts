319846c639e98239f7b57f970d38ac5948983023
Raven Barrogo <barrogoraven@gmail.com>
2024-02-14 12:25:02 +0800

    CHANGE: vue router history :bug:

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/movies', name: 'movies', component: () => import('@/views/MediasView.vue') },
