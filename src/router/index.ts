03a13f0febbc4d177cf38efa869406000ea9bcdd
Raven Barrogo <barrogoraven@gmail.com>
2024-02-14 13:57:38 +0800

    FIX: redirect file :rocket:

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/movies', name: 'movies', component: () => import('@/views/MediasView.vue') },
